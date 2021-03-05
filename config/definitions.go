c04fa2993bc18835c0398b2fb431adcdd73331cc
Johannes Koch <johannes.koch@avenga.com>
2021-03-05 15:13:22 +0100

    saml2 -> saml

--- config/definitions.go
+++ config/definitions.go
@@ -4,5 +4,5 @@ package config
 type Definitions struct {
 	BasicAuth []*BasicAuth `hcl:"basic_auth,block"`
 	JWT       []*JWT       `hcl:"jwt,block"`
-	SAML2     []*SAML2     `hcl:"saml2,block"`
+	SAML      []*SAML      `hcl:"saml,block"`
 }
